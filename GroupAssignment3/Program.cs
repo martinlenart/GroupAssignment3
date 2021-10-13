53825956e110ba688944c096c7f391c85a185a9f
Martin Lenart <martin_lenart@icloud.com>
2021-10-13 10:18:50 +0200

    Add project files.

---
+++ GroupAssignment3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GroupAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentList myStudents = new StudentList();
+            Console.WriteLine("Empty student list");
+            Console.WriteLine(myStudents);
+
+            Console.WriteLine("\nStudent list");
+            myStudents.InitiateOOP1dotNet5();
+            Console.WriteLine(myStudents);
+
+            Console.WriteLine("\nSorted student list");
+            myStudents.Sort();
+            Console.WriteLine(myStudents);
+            Console.WriteLine();
+            
+            int NrOfGroups = 0;
+            bool Continue = UserInput.TryReadInt32("How many groups do you want", 2, myStudents.Count / 2, out NrOfGroups);
+
+            if (!Continue) return;
+
+            myStudents.CreateGroups(NrOfGroups);
+            Console.WriteLine($"If I make {NrOfGroups}, each group will have {myStudents.NrInGroup} students and {myStudents.NrNotInGroup} student remaining");
+
+        }
+
+    }
+
+    //Exercise Sprint 1
+    //1. Properties and Methods of class StudentList
+    //2. How do I keep track of NrOfStudents
+    //3. How do I initiate students in InitiateOOP1dotNet5()
+    //4. What changes do I need to make in "Selection Sort" From BOOP_05_07 to implement Sort()
+}
---
+++ GroupAssignment3/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment3
+{
+    static class UserInput
+    {
+        #region User interaction
+        public  static bool TryReadInt32(string question, int minInt, int maxInt, out int readInt)
+        {
+            readInt = 0;
+            string sInput;
+            do
+            {
+                Console.WriteLine($"{question} ({minInt}-{maxInt} or Q to quit)?");
+                sInput = Console.ReadLine();
+                if (int.TryParse(sInput, out readInt) && readInt >= minInt && readInt <= maxInt)
+                {
+                    return true;
+                }
+                else if (sInput != "Q" && sInput != "q")
+                {
+                    Console.WriteLine("Wrong input, please try again.");
+                }
+            }
+            while ((sInput != "Q" && sInput != "q"));
+            return false;
+        }
+        #endregion
+    }
+}
